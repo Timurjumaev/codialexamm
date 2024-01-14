1bcd3039f7b62dd7d6a83394989fe9c19ae2ac35
Timurjumaev <Tjumaev2002@gmail.com>
2024-01-14 14:25:45 +0500

    1

---
+++ main.py
@@ -0,0 +1,36 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import api
+
+
+description = """
+------------------------------
+**Username and password for Admin**
+* Login: **admin**
+* Parol: **admin123**
+------------------------------
+"""
+
+app = FastAPI(
+    description=description,
+    contact={
+        'name': "Temur Jumayev's telegram account url for questions",
+        'url': 'https://t.me/Temur_Jumayev',
+    },
+    docs_url='/',
+    redoc_url='/redoc',
+)
+
+app.include_router(api)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], )
+
+
+
---
+++ models/only_for_alembic.py
@@ -0,0 +1,9 @@
+from db import Base
+from models.users import Users
+from models.answers import Answers
+from models.categories import Categories
+from models.questions import Questions
+from models.finalresults import FinalResults
+from models.results import Results
+
+
